0818855c3fb0d1cbe6b1b9e1953cf5ead51633d3
PROGR WEB - MANHA <PROGR WEB - MANHA@MININT-IH8VEDN>
2021-01-27 12:11:44 -0300

    IMPLEMENTAÇÃO DO BANCO

---
+++ painel/bd/conexão.php
@@ -0,0 +1,18 @@
+<?php
+
+class conexao {
+
+    private $db_server = 'localhost';
+    private $db_name = 'paineladm';
+    private $db_charset = 'utf8';
+    private $db_username = 'root';
+    private $db_password = '';
+
+    public function selecionarDados($consulta, $parametros = null, $debug = true, $close_connection = true){
+        $result = null;
+        
+        $connection = new PD0(
+                    
+        )                
+    }
+}
